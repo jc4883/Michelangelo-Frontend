27237ea8fcb54a5f78045d3c978ca5284b82d5f7
jc4883 <jc4883@columbia.edu>
2020-10-19 19:47:31 -0400

    Fix break in branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. This was committed by main.
+          Edit <code>src/App.tsx</code> and save to reload. This was committed by feature branch!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          attempt to introduce break.
+          learn react
         </a>
       </header>
     </div>
