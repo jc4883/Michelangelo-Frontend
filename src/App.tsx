102f5046d6b398a70b1171b3f6232a169735a953
jc4883 <jc4883@columbia.edu>
2020-10-19 19:30:05 -0400

    Attempt to commit with failing test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test should fail.
         </a>
       </header>
     </div>
