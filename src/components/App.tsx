2706aed043133dc15156b2a54bbc86db07293669
jc4883 <jc4883@columbia.edu>
2020-10-20 01:52:19 -0400

    Setup absolute pathing, css modules, redux

---
+++ src/components/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "redux/configureStore";
+
+
+import SplashPage from './splash_page/SplashPage';
+
+import "./App.scss";
+
+export const store = configureStore();
+
+const App = () => {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={SplashPage} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/splash_page/SplashPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './SplashPage.module.scss';
+
+const SplashPage = () => {
+    return (
+        <div className={styles.red}>
+            Welcome to translator chat app!
+        </div>
+    )
+}
+
+export default SplashPage
