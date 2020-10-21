86a1d1c6bdf2bf9d4228d92df6ffc77c4ba3c384
jc4883 <jc4883@columbia.edu>
2020-10-20 20:56:09 -0400

    Setup splash page

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,20 +3,19 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "redux/configureStore";
 
-import SplashPage from "./splash_page/SplashPage";
-
-import "./App.scss";
+import SplashPage from "../components/SplashPage";
+import MainPage from "./MainPage";
 
 export const store = configureStore();
 
 const App = () => {
   return (
     <div className="App">
-      I am here.
       <Provider store={store}>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={SplashPage} />
+            <Route exact path="/splash" component={SplashPage} />
+            <Route exact path="/" component={MainPage} />
           </Switch>
         </BrowserRouter>
       </Provider>
